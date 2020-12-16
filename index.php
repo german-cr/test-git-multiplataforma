55a7a0b40201741823acdd7e72bb874d58e99f7a
German Contreras Roco [lnx] <gecontreras@gmail.com>
2020-12-16 01:25:57 -0300

    agrega linea en index, listo para version

--- index.php
+++ index.php
@@ -4,4 +4,6 @@ echo 'esta es la primera linea de codigo';
 echo 'esta es la segunda linea de codigo';
 echo 'esta es la tercera linea de codigo';
 
+echo 'esta es la linea para la proxima version preliminar 0.2';
+
 ?>
